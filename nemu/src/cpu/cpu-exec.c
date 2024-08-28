e0a28eed92ed2771b2d24d627b5bc3b431991731
tracer-ics2023 <tracer@njuics.org>
2024-08-28 10:54:10 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux ubuntu 5.15.0-113-generic #123~20.04.1-Ubuntu SMP Wed Jun 12 17:33:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:54:10 up  6:03,  1 user,  load average: 0.08, 0.05, 0.01

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -99,7 +99,6 @@ void assert_fail_msg() {
 /* Simulate how the CPU works. */
 void cpu_exec(uint64_t n) {
   g_print_step = (n < MAX_INST_TO_PRINT);
-  printf("n=%lu\n",n);
   switch (nemu_state.state) {
     case NEMU_END: case NEMU_ABORT:
       printf("Program execution has ended. To restart the program, exit NEMU and run again.\n");
--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -20,9 +20,6 @@ NEMUState nemu_state = { .state = NEMU_STOP };
 
 int is_exit_status_bad() {
   int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
-    (nemu_state.state == NEMU_QUIT);
-  printf("nemu_state.state=%d\n",nemu_state.state);
-  printf("halt_ret=%d\n",nemu_state.halt_ret);
-  printf("good=%d\n",good);
+    (nemu_state.state == NEMU_END);
   return !good;
 }
