58904daa2493563d7382e47639549da61448aa5b
tracer-ics2023 <tracer@njuics.org>
2024-08-28 15:24:58 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux ubuntu 5.15.0-113-generic #123~20.04.1-Ubuntu SMP Wed Jun 12 17:33:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:24:58 up  9:02,  1 user,  load average: 0.20, 0.05, 0.02

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -55,7 +55,11 @@ static int cmd_q(char *args) {
 static int cmd_help(char *args);
 
 static int cmd_si(char *args) {
-  printf("%s\n",args);
+  if(args){
+    printf("%s\n",args);
+  }else{
+    printf("No args provided.\n");
+  }
   return 0;
 }
 
