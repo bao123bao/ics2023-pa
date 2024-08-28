e2cc3cd8168d8a6a157c8a8ecb99ed8a2075b455
tracer-ics2023 <tracer@njuics.org>
2024-08-28 15:44:26 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux ubuntu 5.15.0-113-generic #123~20.04.1-Ubuntu SMP Wed Jun 12 17:33:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:44:26 up  9:21,  1 user,  load average: 0.10, 0.06, 0.03

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -79,7 +79,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-  { "si [N]", "Execute a step of instruction for N times", cmd_si}
+  { "si", "Execute a step of instruction for N times", cmd_si}
 
   /* TODO: Add more commands */
 
