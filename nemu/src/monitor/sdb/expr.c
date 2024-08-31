7cc3da6a00a1aff16aefb4bbbc5e074775fe9bb4
tracer-ics2023 <tracer@njuics.org>
2024-08-31 20:09:32 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux ubuntu 5.15.0-113-generic #123~20.04.1-Ubuntu SMP Wed Jun 12 17:33:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:09:32 up 10:11,  8 users,  load average: 0.05, 0.08, 0.02

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -207,8 +207,8 @@ int op_position(int p, int q) {
 
 		cur_type = tokens[p].type;
 
-		if(debug_flag && cur_op_type>0)
-			printf("p=%d, cur_type=%d, cur_op='%c'@%d\n", p, cur_type,cur_op_type, cur_op_pos );
+		if(debug_flag && cur_op_type>0 && cur_type<256)
+			printf("p=%d, cur_type=%c, cur_op='%c'@%d\n", p, cur_type,cur_op_type, cur_op_pos );
 
 		switch (cur_type) {
 
