4182cba67cc5f8927c189163c8dfc04a818132c9
tracer-ics2023 <tracer@njuics.org>
2024-08-31 16:58:33 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux ubuntu 5.15.0-113-generic #123~20.04.1-Ubuntu SMP Wed Jun 12 17:33:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:58:33 up  7:00,  5 users,  load average: 0.01, 0.03, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -325,7 +325,7 @@ word_t expr(char *e, bool *success) {
 	size_t len = 0;
 	ssize_t read;
 
-	fp = fopen("./output", "r");
+	fp = fopen("~/ics-2023/nemu/src/monitor/sdb/output", "r");
 	if (!fp) {
 		printf("No file found\n");
 		return 0;
