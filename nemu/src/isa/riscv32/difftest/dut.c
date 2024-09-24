a5e70acecb98adc61009fb82f1d719f299997cf1
tracer-ics2023 <tracer@njuics.org>
2024-09-24 15:16:22 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux my-ubuntu 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:16:22 up 1 day, 15:21, 23 users,  load average: 0.13, 0.13, 0.16

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -17,13 +17,31 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
+const char *regs_r[] = {
+  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+};
+
+void isa_reg_r_display(CPU_state *ref_r) {
+	int i;
+	printf("nemu vs ref gpr values:\n");
+	printf("id 0x%-20s 0x%-20s\n", "nemu (dut)", "ref");
+	for (i=0; i<32; i++){
+		printf("[%2d]%-7s 0x%-20x 0x%-20x\n", i, regs_r[i], cpu.gpr[i], ref_r->gpr[i]);
+	}
+	printf("%-7s 0x%-20x\n 0x%-20x", "pc", cpu.pc, ref_r->pc);
+}
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   // pc is current instruction's address
 	// ref_r->pc is dnpc value updated by ref
 	int i;
 	for(i=0; i<32; i++){
 		if(ref_r->gpr[i] != cpu.gpr[i]){
-			printf("Error at pc=0x%x, gpr[%d] check failed\n",pc, i);
+			printf("Error at pc=0x%x, gpr[%d] <%s>  check failed\n",pc, i, regs_r[i]);
+			isa_reg_r_display(ref_r);
 			return false;
 		}
 	}
