68cd5b94b5cd9dd1fe97a603f42e0b06bc9970b7
tracer-ics2023 <tracer@njuics.org>
2024-09-27 09:40:21 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux my-ubuntu 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:40:21 up 2 days,  3:56, 23 users,  load average: 0.85, 0.91, 0.71

--- nemu/src/device/keyboard.c
+++ nemu/src/device/keyboard.c
@@ -86,7 +86,8 @@ static void i8042_data_io_handler(uint32_t offset, int len, bool is_write) {
   assert(!is_write);
   assert(offset == 0);
   i8042_data_port_base[0] = key_dequeue();
-	Log("kbd base written %d\n", i8042_data_port_base[0]);
+	if(i8042_data_port_base[0])
+		Log("kbd base written %d\n", i8042_data_port_base[0]);
 }
 
 void init_i8042() {
