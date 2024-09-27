bbc1ee1bdccad77d4ba821441c13e57b4ca7a89b
tracer-ics2023 <tracer@njuics.org>
2024-09-27 09:38:52 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux my-ubuntu 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:38:51 up 2 days,  3:55, 23 users,  load average: 0.82, 0.81, 0.65

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,12 +5,12 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
 	
-	kbd->keycode = 0;
-	kbd->keydown = 0;
-	/*if(kbd->keycode != 0){
+	kbd->keycode = inl(KBD_ADDR);
+//	kbd->keydown = 0;
+	if(kbd->keycode != 0){
 		kbd->keydown = 1;
 	}else{
 		kbd->keydown = 0;
 	}
-	*/
+	
 }
