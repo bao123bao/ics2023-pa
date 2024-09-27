8e4f4edd711d7cd6c0f19629ddfa36c31a213ec4
tracer-ics2023 <tracer@njuics.org>
2024-09-27 12:27:01 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux my-ubuntu 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:27:01 up 2 days,  4:30, 23 users,  load average: 0.22, 0.25, 0.28

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,13 +1,15 @@
 #include <am.h>
 #include <nemu.h>
-
+#include <stdio.h>
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-	kbd->keycode = 0;
+	kbd->keycode = AM_KEY_NONE;
 	kbd->keydown = 0;
 	
 	uint32_t scancode = inl(KBD_ADDR);
+	if(scancode)
+		printf("scancode: 0x%x\n", scancode);
 	kbd->keycode = scancode & (KEYDOWN_MASK - 1);
 	kbd->keydown = scancode & KEYDOWN_MASK;
 	
