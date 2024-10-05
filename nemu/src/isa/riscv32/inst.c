213656dd374d2df790a79c37838fe99606c846ee
tracer-ics2023 <tracer@njuics.org>
2024-10-05 20:49:30 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux my-ubuntu 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:49:30 up 3 days, 14:50, 29 users,  load average: 1.02, 0.79, 0.41

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -148,6 +148,23 @@ static int decode_exec(Decode *s) {
 	INSTPAT("0000001 ????? ????? 110 ????? 01100 11", rem    , R, R(rd) = (int32_t)src1 % (int32_t)src2);
 	INSTPAT("0000001 ????? ????? 111 ????? 01100 11", remu   , R, R(rd) = (uint32_t)src1 % (uint32_t)src2);
 
+	INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw  , I, 
+		word_t *csrp;
+		switch(imm){
+			case 0x300: csrp = &cpu.mstatus; break;
+			case 0x305: csrp = &cpu.mtvec;   break;
+			case 0x341: csrp = &cpu.mepc;    break;
+			case 0x342: csrp = &cpu.mcause;  break;
+			default: printf("csr idx error\n"); assert(0);
+		}
+
+		if(rd!=0){
+			R(rd) = *csrp;
+		}
+
+		*csrp = src1;
+	);	
+
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
