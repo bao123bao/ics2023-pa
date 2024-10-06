8d565af9aa98cab8707a7696a3e16fc860c0fa55
tracer-ics2023 <tracer@njuics.org>
2024-10-06 09:41:01 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux my-ubuntu 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:41:01 up 3 days, 16:35, 29 users,  load average: 1.28, 0.87, 0.48

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -186,8 +186,8 @@ static int decode_exec(Decode *s) {
 
 	INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, 
 		printf("ecall called\n");
-		printf("ecall event = %d\n", 1);
-		s->dnpc = isa_raise_intr(1, s->pc);
+		printf("ecall event = %d\n", 5);
+		s->dnpc = isa_raise_intr(5, s->pc);
 	);
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
