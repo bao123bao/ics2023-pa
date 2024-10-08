98edf7a7b35622942797298da9293d87eeb41430
tracer-ics2023 <tracer@njuics.org>
2024-10-08 11:59:24 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux my-ubuntu 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:59:24 up 4 days,  3:50, 29 users,  load average: 1.05, 0.66, 0.38

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -30,7 +30,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 	// reset bit 7
 	cpu.mstatus &= ~mask_MPIE;
 
-	printf("riase NO = %d, cpu.mcause=%d,  epc=%d\n", NO, cpu.mcause, epc);
+	//printf("riase NO = %d, cpu.mcause=%d,  epc=%d\n", NO, cpu.mcause, epc);
 
   return cpu.mtvec;
 }
