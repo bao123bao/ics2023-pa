727a72830b27e038d37dfc169c170211961bc7a6
tracer-ics2023 <tracer@njuics.org>
2024-10-09 14:28:12 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux my-ubuntu 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:28:12 up 4 days,  7:33, 31 users,  load average: 0.49, 0.74, 0.60

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include <proc.h>
 #include <elf.h>
+#include <assert.h>
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -9,8 +10,31 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+//extern uint8_t ramdisk_start;
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
+
+	Elf_Ehdr ehdr;
+	ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+	
+
+	assert(*(uint32_t *)ehdr.e_ident == 0x7f454c46);
+	printf("assert passed\n");
+/*
+	Elf_Phdr *phdr = (Elf_Phdr*) (&ramdisk_start + ehdr->e_phoff);
+
+	uint16_t phnum = ehdr->e_phnum;
+	uint16_t phentsize = ehdr->e_phentsize;
+	printf("phnum=%d, phentsize=%d\n", phnum, phentsize);
+	int i;
+	for(i=0; i<phnum; i++){
+		printf("[%2d] type=%d, offset=%d, vaddr=%d\n", 
+			phdr[i].p_type, phdr[i].p_offset, phdr[i].p_vaddr);
+
+	}
+
+*/
   return 0;
 }
 
