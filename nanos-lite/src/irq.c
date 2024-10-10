e8de2edc48a767cfcb980890c1b3ee72ee10e819
tracer-ics2023 <tracer@njuics.org>
2024-10-10 20:10:25 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux my-ubuntu 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:10:25 up 4 days, 19:32, 37 users,  load average: 0.66, 0.33, 0.25

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,7 +4,7 @@ static Context* do_event(Event e, Context* c) {
 	switch (e.event) {
     case EVENT_YIELD: printf("nanos: yield event\n"); break;
 		case EVENT_SYSCALL:
-			printf("nanos: syscall event\n"); 
+			//printf("nanos: syscall event\n"); 
 			do_syscall(c);
 			break;
 		default: panic("Unhandled event ID = %d", e.event);
