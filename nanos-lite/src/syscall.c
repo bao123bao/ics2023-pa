5036a7ddc37d4f971b4ef34746f0a2771d5ed083
tracer-ics2023 <tracer@njuics.org>
2024-10-10 20:09:29 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux my-ubuntu 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:09:29 up 4 days, 19:31, 37 users,  load average: 0.52, 0.22, 0.21

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -13,6 +13,20 @@ int sys_exit(int status) {
 	return status;
 }
 
+int sys_write(int fd, const void *buf, size_t count) {
+	if(fd==1 || fd==2){
+		int i;
+		char *chars = (char *)buf;
+		int cnt = 0;
+		for(i=0; i<count; i++){
+			putch(chars[i]);
+			cnt++;
+		}
+		return cnt;
+	}
+	return -1;
+}
+
 void do_syscall(Context *c) {
   int ret_val = 0;
 //	printf("STRACE=%d\n",CONFIG_STRACE);
@@ -36,6 +50,14 @@ void do_syscall(Context *c) {
 			ret_val = sys_exit(a[1]);
 #ifdef CONFIG_STRACE
 			printf("syscall: exit (args: %d, %d, %d, ret_val=%d)\n", 
+				a[1], a[2], a[3], ret_val);
+#endif
+			break;
+		
+		case SYS_write:
+			ret_val = sys_write(a[1], (void *)a[2], a[3]);
+#ifdef CONFIG_STRACE
+			printf("syscall: write (args: %d, %d, %d, ret_val=%d)\n", 
 				a[1], a[2], a[3], ret_val);
 #endif
 			break;
