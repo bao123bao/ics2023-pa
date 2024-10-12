8314ef80e13b5c8076c43bffc5654ccb5cc4c6af
tracer-ics2023 <tracer@njuics.org>
2024-10-12 10:36:13 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux my-ubuntu 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:36:13 up 5 days,  6:23, 37 users,  load average: 0.73, 0.47, 0.48

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include <proc.h>
 #include <elf.h>
+#include <fs.h>
 //#include <assert.h>
 
 #ifdef __LP64__
@@ -13,37 +14,47 @@
 //extern uint8_t ramdisk_start;
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
+int fs_open(const char *pathname, int flags, int mode);
+size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len);
+size_t fs_lseek(int fd, size_t offset, int whence);
+int fs_close(int fd);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
 	
-	Elf_Ehdr ehdr;
-	ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+	int fd = fs_open(filename, 0, 0);
 	
-	uint16_t phnum = ehdr.e_phnum;
-//	uint16_t phentsize = ehdr.e_phentsize;
+	Elf_Ehdr ehdr;
+//	ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+	
+	size_t rd_len = fs_read(fd, &ehdr, sizeof(Elf_Ehdr));
+	assert(rd_len == sizeof(Elf_Ehdr));
 
-//	assert(*(uint32_t *)ehdr.e_ident == 0x7f454c46);
-//	printf("assert passed\n");
+	uint16_t phnum = ehdr.e_phnum;
 
 	Elf_Phdr phdr[phnum];
-	ramdisk_read(&phdr, 0 + ehdr.e_phoff, phnum*sizeof(Elf_Phdr));
+	
+	int seek_ret = fs_lseek(fd, ehdr.e_phoff, SEEK_SET);
+	assert(seek_ret == ehdr.e_phoff);
 
-//	printf("phnum=%d, phentsize=%d\n", phnum, phentsize);
+	rd_len = fs_read(fd, &phdr, phnum*sizeof(Elf_Phdr));
+	assert(rd_len == phnum*sizeof(Elf_Phdr));
+//	ramdisk_read(&phdr, 0 + ehdr.e_phoff, phnum*sizeof(Elf_Phdr));
+
 	int i;
 	for(i=0; i<phnum; i++){
-//		printf("[%d] type=%d, offset=%d, vaddr=%d\n", 
-//			i, phdr[i].p_type, phdr[i].p_offset, phdr[i].p_vaddr);
 		if (phdr[i].p_type == PT_LOAD) {
 			
-			// copy from ramrisk to memory
-			ramdisk_read((void *)phdr[i].p_vaddr, 0 + phdr[i].p_offset, phdr[i].p_filesz);
+			fs_lseek(fd, phdr[i].p_offset, SEEK_SET);
+			fs_read(fd, (void *)phdr[i].p_vaddr, phdr[i].p_filesz);
+			//ramdisk_read((void *)phdr[i].p_vaddr, 0 + phdr[i].p_offset, phdr[i].p_filesz);
 			
 			// set zeros
 			memset((void *)phdr[i].p_vaddr + phdr[i].p_filesz, 0, phdr[i].p_memsz - phdr[i].p_filesz);
-
-			printf("load the above\n");
-
 		}
 	}
+	fs_close(fd);
+
 	return ehdr.e_entry;
 }
 
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,6 +3,16 @@
 
 #define CONFIG_STRACE 1
 
+
+int fs_open(const char *pathname, int flags, int mode);
+size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len);
+size_t fs_lseek(int fd, size_t offset, int whence);
+int fs_close(int fd);
+
+
+
+
 int sys_yield() {
 	yield();
 	return 0;
@@ -14,6 +24,9 @@ int sys_exit(int status) {
 }
 
 int sys_write(int fd, const void *buf, size_t count) {
+	
+	return fs_write(fd, buf, count);
+/*
 	if(fd==1 || fd==2){
 		int i;
 		char *chars = (char *)buf;
@@ -25,6 +38,7 @@ int sys_write(int fd, const void *buf, size_t count) {
 		return cnt;
 	}
 	return -1;
+	*/
 }
 
 extern char _end;
@@ -33,9 +47,29 @@ int sys_brk(void *addr){
 	return 0;
 }
 
+int sys_open(const char *pathname, int flags, int mode) {
+	return fs_open(pathname, flags, mode);
+}
+
+int sys_read(int fd, void *buf, size_t count) {
+	return fs_read(fd, buf, count);
+}
+/*
+int sys_write(int fd, const void *buf, size_t count) {
+	return fs_write(fd, buf, count);
+}
+*/
+int sys_lseek(int fd, size_t offset, int whence) {
+	return fs_lseek(fd, offset, whence);
+}
+
+int sys_close(int fd) {
+	return fs_close(fd);
+}
+
 void do_syscall(Context *c) {
   int ret_val = 0;
-//	printf("STRACE=%d\n",CONFIG_STRACE);
+	
 	uintptr_t a[4];
   a[0] = c->GPR1;
 	a[1] = c->GPR2;
@@ -45,28 +79,26 @@ void do_syscall(Context *c) {
   switch (a[0]) {
 		case SYS_brk:
 			ret_val = sys_brk((void *)a[1]);
+			//char *s = 
 #ifdef CONFIG_STRACE
-			printf("syscall: brk (args: %d, %d, %d, ret_val=%d)\n", 
-				a[1], a[2], a[3], ret_val);
+			printf("syscall: brk (args: %d, ret_val=%d)\n", a[1], ret_val);
 #endif
 			break;
 
 		case SYS_yield:
 			ret_val = sys_yield();
 #ifdef CONFIG_STRACE
-			printf("syscall: yield (args: %d, %d, %d, ret_val=%d)\n", 
-				a[1], a[2], a[3], ret_val);
+			printf("syscall: yield (args: none, ret_val=%d)\n",  ret_val);
 #endif
 			break;
 
 		case SYS_exit:
 			ret_val = sys_exit(a[1]);
 #ifdef CONFIG_STRACE
-			printf("syscall: exit (args: %d, %d, %d, ret_val=%d)\n", 
-				a[1], a[2], a[3], ret_val);
+			printf("syscall: exit (args: %d, ret_val=%d)\n", a[1], ret_val);
 #endif
 			break;
-		
+		/*
 		case SYS_write:
 			ret_val = sys_write(a[1], (void *)a[2], a[3]);
 #ifdef CONFIG_STRACE
@@ -74,7 +106,46 @@ void do_syscall(Context *c) {
 				a[1], a[2], a[3], ret_val);
 #endif
 			break;
+		*/
+		case SYS_open:
+			ret_val = sys_open((const char *)a[1], a[2], a[3]);
+#ifdef CONFIG_STRACE
+			printf("syscall: open (args: %d, %d, %d, ret_val=%d)\n", 
+				a[1], a[2], a[3], ret_val);
+#endif
+			break;
 
+		case SYS_read:
+			ret_val = sys_read(a[1], (void *)a[2], a[3]);
+#ifdef CONFIG_STRACE
+			printf("syscall: read (args: %d, %d, %d, ret_val=%d)\n", 
+				a[1], a[2], a[3], ret_val);
+#endif
+			break;
+
+		case SYS_write:
+			ret_val = sys_write(a[1], (void *)a[2], a[3]);
+#ifdef CONFIG_STRACE
+			printf("syscall: write (args: %d, %d, %d, ret_val=%d)\n", 
+				a[1], a[2], a[3], ret_val);
+#endif
+			break;
+		
+		case SYS_lseek:
+			ret_val = sys_lseek(a[1], a[2], a[3]);
+#ifdef CONFIG_STRACE
+			printf("syscall: lseek (args: %d, %d, %d, ret_val=%d)\n", 
+				a[1], a[2], a[3], ret_val);
+#endif
+			break;
+
+		case SYS_close:
+			ret_val = sys_close(a[1]);
+#ifdef CONFIG_STRACE
+			printf("syscall: lseek (args: %d, ret_val=%d)\n", a[1], ret_val);
+#endif
+			break;
+			
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 	// set a0 to return value
