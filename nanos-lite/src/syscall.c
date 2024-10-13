4d9c696c700ed1db5ea29c7cb8129ac723981bd5
tracer-ics2023 <tracer@njuics.org>
2024-10-13 22:36:57 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux my-ubuntu 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:36:57 up  5:05, 12 users,  load average: 0.21, 0.29, 0.25

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -28,9 +28,10 @@ int sys_gettimeofday(struct timeval *tv, struct timezone *tz) {
 
 	AM_TIMER_UPTIME_T uptime = io_read(AM_TIMER_UPTIME);
 	uint64_t us = uptime.us;
-//	printf("uptime.us=%d\n", (int)us);
 	tv->tv_sec  = us / 1000000;
 	tv->tv_usec = us % 1000000;
+	printf("uptime.us=%d, tv_sec=%d, tv_usec=%d\n", 
+		(int)us, (int)tv->tv_sec, (int)tv->tv_usec);
 	return 0;
 }
 
