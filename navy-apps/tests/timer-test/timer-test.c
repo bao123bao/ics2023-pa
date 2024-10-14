8c67b14eaf2c47f8103142027fb9843c1a6b74d0
tracer-ics2023 <tracer@njuics.org>
2024-10-14 09:40:47 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux my-ubuntu 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:40:47 up  5:30, 12 users,  load average: 0.81, 0.56, 0.48

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -11,8 +11,9 @@ int main() {
 	while (1) {
 		gettimeofday(&tv, NULL);
 	//	printf("tv-last = %d\n", tv.tv_usec - last_tv.tv_usec);
-    if (tv.tv_usec - last_tv.tv_usec >= 500000) {
+    if ( (tv.tv_sec-last_tv.tv_sec)*1000000 + (tv.tv_usec - last_tv.tv_usec) >= 500000) {
   	  printf("Hello Timer from Navy-apps @ %d sec %d usec\n", (int)tv.tv_sec, (int)tv.tv_usec);
+			//gettimeofday(&tv, NULL);
 			last_tv = tv;
     }
   }
