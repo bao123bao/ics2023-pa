9a6912aaab85cee7e897003464e0dda6ccbf2938
tracer-ics2023 <tracer@njuics.org>
2024-10-14 11:44:14 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux my-ubuntu 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:44:14 up  7:29, 13 users,  load average: 0.96, 0.93, 0.83

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -24,7 +24,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-
+	printf("in events_read\n");
 	AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
 //	char temp[100];
 	int cnt = 0;
