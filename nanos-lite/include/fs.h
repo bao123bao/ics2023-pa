f130afdc9cd9c5946492b7f7436edd765ad1740a
tracer-ics2023 <tracer@njuics.org>
2024-10-14 12:30:19 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux my-ubuntu 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:30:19 up  7:45, 13 users,  load average: 0.57, 0.51, 0.64

--- nanos-lite/include/fs.h
+++ nanos-lite/include/fs.h
@@ -7,7 +7,7 @@
 enum {SEEK_SET, SEEK_CUR, SEEK_END};
 #endif
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENT};
+enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_EVENT, FD_FB};
 
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
