b5b063c52e87baf4e5d394e91454e6fdfe7f4702
tracer-ics2023 <tracer@njuics.org>
2024-10-16 21:14:10 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux my-ubuntu 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:14:10 up 1 day, 58 min, 14 users,  load average: 2.43, 2.15, 1.86

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -29,7 +29,7 @@ int SDL_WaitEvent(SDL_Event *event) {
 			break;
 	}
 
-	printf("SDL read from NDL, cnt=%d: %s\n", cnt, buf);
+//	printf("SDL read from NDL, cnt=%d: %s\n", cnt, buf);
 	
 	// get key up or down
 	key_act = buf[1];
@@ -42,28 +42,27 @@ int SDL_WaitEvent(SDL_Event *event) {
 		sscanf(buf, "kd %s", namebuf);
 		//namebuf = buf + 3;
 	}else{
-		printf("SDL: key act type error!\n");
+	//	printf("SDL: key act type error!\n");
 		assert(0);
 	}
 	
-	printf("keyname=%s\n",namebuf);
+	//printf("keyname=%s\n",namebuf);
 	
 	int i, nlen;
 	nlen = sizeof(keyname) / sizeof(char*);
-	printf("nlen=%d\n", nlen);
+	//printf("nlen=%d\n", nlen);
 
 	for(i=0; i<nlen; i++){
 //		printf("keyname[%d]=%s vs namebuf=%s\n", i, keyname[i], namebuf);
 		if(strcmp(keyname[i], namebuf) == 0){
 			assert(i != 0);
 			event->key.keysym.sym = i;
-			printf("SDL key catch: %s[%c]\n", namebuf, key_act);
+			//printf("SDL key catch: %s[%c]\n", namebuf, key_act);
 			return 1;
 		}
 	}
 
 	printf("no keyname matched\n");
-
 	return 0;
 }
 
