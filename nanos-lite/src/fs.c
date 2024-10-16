defb4ee644c6846852800f9b0f03154631843ccb
tracer-ics2023 <tracer@njuics.org>
2024-10-16 22:04:38 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux my-ubuntu 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:04:38 up 1 day,  1:37, 13 users,  load average: 1.35, 1.33, 1.50

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -52,11 +52,15 @@ void init_fs() {
 int fs_open(const char *pathname, int flags, int mode) {
 	// orinary files
 	printf("fs_open: <%s>\n", pathname);
+	
+
 	int i;
 	int files_num = sizeof(file_table) / sizeof(Finfo);
 	for (i=0; i<files_num; i++){
 		//printf("%s vs %s\n", file_table[i].name, pathname);
 		if ( strcmp(file_table[i].name, pathname) == 0 ){
+			// reset open_offset
+			open_offsets[i] = 0;
 			return i;
 		}
 	}
