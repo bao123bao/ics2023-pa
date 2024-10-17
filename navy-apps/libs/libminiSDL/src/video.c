aee8e68b913840da3a05bf519b4ef7c3f3e33328
tracer-ics2023 <tracer@njuics.org>
2024-10-17 15:29:07 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux my-ubuntu 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:29:07 up 1 day,  5:15, 11 users,  load average: 1.88, 2.34, 2.30

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -6,7 +6,7 @@
 #include <stdio.h>
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
 	
-	printf("in sdl blit\n");
+	//printf("in sdl blit\n");
 	// ensure the validity of args
 	assert(dst && src);
   assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
