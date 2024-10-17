2d1301d5fd6d1de321d8436528e7c233df6543bc
tracer-ics2023 <tracer@njuics.org>
2024-10-17 15:07:57 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux my-ubuntu 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:07:56 up 1 day,  4:54, 12 users,  load average: 1.55, 1.69, 1.70

--- navy-apps/tests/sdl-v-test/main.cpp
+++ navy-apps/tests/sdl-v-test/main.cpp
@@ -15,6 +15,8 @@
 #include <SDL_bmp.h>
 #include <SDL_bdf.h>
 
+static SDL_Surface *screen = NULL;
+static SDL_Surface *logo_sf = NULL;
 
 static void clear_display(void) {
   SDL_FillRect(screen, NULL, 0xffffff);
@@ -23,22 +25,25 @@ static void clear_display(void) {
 int main(int argc, char *argv[], char *envp[]) {
   SDL_Init(0);
   screen = SDL_SetVideoMode(0, 0, 32, SDL_HWSURFACE);
+  printf("MAIN: screen size=(%d,%d)\n",screen->w,screen->h);
 
   logo_sf = SDL_LoadBMP("/share/pictures/projectn.bmp");
   assert(logo_sf);
-  set_i_max();
-	
-	//display_menu(1);
-	
+
   while (1) {
   	printf("navy: new while loop\n");		
 		clear_display();
 		SDL_Rect rect = { .x = screen->w - logo_sf->w, .y = 0 };
 		
-		printf("display: call blit\n");
+		printf("navy: call blit\n");
 		SDL_BlitSurface(logo_sf, NULL, screen, &rect);
-		
-		printf("Available applications:\n");
+		printf("navy: call updaterect\n");
+		SDL_UpdateRect(screen, 0, 0, 0, 0);
+		printf("navy: updaterect done\n");
+
+  	printf("========================================\n");
+
+  	fflush(stdout);
 		
     SDL_Event e;
     do {
@@ -58,46 +63,3 @@ int main(int argc, char *argv[], char *envp[]) {
   return -1;
 }
 
-static void display_menu(int n) {
-  clear_display();
-  SDL_Rect rect = { .x = screen->w - logo_sf->w, .y = 0 };
-  printf("display: call blit\n");
-	SDL_BlitSurface(logo_sf, NULL, screen, &rect);
-  printf("Available applications:\n");
-  /*
-	char buf[80];
-  int i;
-  for (i = 0; i <= n; i ++) {
-    auto *item = &items[page * 10 + i];
-    sprintf(buf, "  [%d] %s", i, item->name);
-    draw_text_row(buf, i);
-  }
-
-  i = 11;
-
-  sprintf(buf, "  page = %2d, #total apps = %d", page, nitems);
-  draw_text_row(buf, i);
-  i ++;
-
-  sprintf(buf, "  help:");
-  draw_text_row(buf, i);
-  i ++;
-
-  sprintf(buf, "  <-  Prev Page");
-  draw_text_row(buf, i);
-  i ++;
-
-  sprintf(buf, "  ->  Next Page");
-  draw_text_row(buf, i);
-  i ++;
-
-  sprintf(buf, "  0-9 Choose");
-  draw_text_row(buf, i);
-  i ++;
-*/
-  SDL_UpdateRect(screen, 0, 0, 0, 0);
-
-  printf("========================================\n");
-  printf("Please Choose.\n");
-  fflush(stdout);
-}
