644262413775130ae5848523e3be9cbfdc6e782a
tracer-ics2023 <tracer@njuics.org>
2024-10-17 16:58:37 +0800

    >  compile NEMU 502024270085 GuoXiaomi Linux my-ubuntu 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:58:37 up 1 day,  6:28, 11 users,  load average: 1.77, 1.57, 1.49

--- navy-apps/tests/fixedpt-test/main.c
+++ navy-apps/tests/fixedpt-test/main.c
@@ -6,9 +6,10 @@ int main() {
   fixedpt a = fixedpt_rconst(1.2);
 	fixedpt b = fixedpt_fromint(10);
 	int c = 0;
-	if (b > fixedpt_rconst(7.9)) {
-		c = fixedpt_toint(fixedpt_div(fixedpt_mul(a + FIXEDPT_ONE, b), 		 fixedpt_rconst(2.3)));
-	}
+	//if (b > fixedpt_rconst(7.9)) {
+		//c = fixedpt_toint(fixedpt_div(fixedpt_mul(a + FIXEDPT_ONE, b), 		 fixedpt_rconst(2.3)));
+	//}
+	c = fixedpt_mul(a, b);
 	printf("c=0x%x\n", c);
   return 0;
 }
